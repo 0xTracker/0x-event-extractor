9cce2ce468f331a8fbc811187afb7022a0d0458c
Craig Bovis <craig@bovis.me.uk>
2019-10-05 15:26:36 +0100

    Add comment

--- packages/shared/src/web3.js
+++ packages/shared/src/web3.js
@@ -5,6 +5,13 @@ const { providerUtils } = require('@0x/utils');
 let wrapper;
 let providerEngine;
 
+/**
+ * Initializes a web3 provider engine and create a reusable web3 wrapper. The
+ * singletons can be accessed via getProviderEngine and getWrapper respectively.
+ *
+ * @param {Object} options - Configuration options.
+ * @param {string} options.endpoint - RPC endpoint of an Ethereum node.
+ */
 const configure = ({ endpoint }) => {
   providerEngine = new Web3ProviderEngine();
   wrapper = new Web3Wrapper(providerEngine);
