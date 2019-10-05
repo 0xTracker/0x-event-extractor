abd17e00abdf73d1e07a098128e44a27c8eef6c1
Craig Bovis <craig@bovis.me.uk>
2019-10-05 15:14:22 +0100

    Ensure 0x.js doesn't make redundant calls

--- packages/shared/src/web3.js
+++ packages/shared/src/web3.js
@@ -1,16 +1,16 @@
 const { RPCSubprovider, Web3ProviderEngine } = require('@0x/subproviders');
 const { Web3Wrapper } = require('@0x/web3-wrapper');
+const { providerUtils } = require('@0x/utils');
 
 let wrapper;
 let providerEngine;
 
 const configure = ({ endpoint }) => {
   providerEngine = new Web3ProviderEngine();
+  wrapper = new Web3Wrapper(providerEngine);
 
   providerEngine.addProvider(new RPCSubprovider(endpoint));
-  providerEngine.start();
-
-  wrapper = new Web3Wrapper(providerEngine);
+  providerUtils.startProviderEngine(providerEngine);
 };
 
 const getWrapper = () => wrapper;
