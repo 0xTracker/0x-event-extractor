e5fb642e7286adb2c0797175d976a61bdd8a5328
Craig Bovis <craig@bovis.me.uk>
2019-11-13 18:22:50 +0000

    Increase job runner resiliency

--- packages/core/src/util/job-runner.js
+++ packages/core/src/util/job-runner.js
@@ -3,23 +3,29 @@ const withRetry = require('promise-poller').default;
 
 const logger = require('./logger');
 
-const repeatTask = (task, interval, maxRetries) =>
+const repeatTask = (task, { minInterval, maxInterval }) =>
   withRetry({
-    taskFn: task,
-    interval,
-    retries: maxRetries,
+    max: maxInterval,
+    min: minInterval,
     progressCallback: (retriesRemaining, error) => logger.logError(error),
+    retries: 999999, // Setting a large number because poller does not work properly with Infinity
+    strategy: 'exponential-backoff',
+    taskFn: task,
   })
-    .then(() => delay(interval))
-    .then(() => repeatTask(task, interval))
-    .catch(() => {
-      logger.logError(
-        `Stopped running ${task.name} due to too many failures (${maxRetries}).`,
-      );
+    .then(() => delay(minInterval))
+    .then(() => repeatTask(task, { maxInterval, minInterval }))
+    .catch(error => {
+      logger.logError(error);
+      logger.logError(`Stopped running ${task.name}.`);
     });
 
 const runJobs = jobs => {
-  jobs.forEach(job => repeatTask(job.fn, job.interval, job.maxRetries));
+  jobs.forEach(job =>
+    repeatTask(job.fn, {
+      minInterval: job.minInterval,
+      maxInterval: job.maxInterval,
+    }),
+  );
 };
 
 module.exports.runJobs = runJobs;
