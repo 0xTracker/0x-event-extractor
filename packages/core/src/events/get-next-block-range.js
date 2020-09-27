6b38c28e34dfec05794fc086b9b9f580a41cfac3
Craig Bovis <craig@bovis.me.uk>
2020-09-27 19:19:19 +0200

    Introduce Uniswap V2 event extractor (#379)
    * Introduce Uniswap V2 event extractor
    
    * Recursively fetch all pages of Uniswap events
    
    * Tweak config
    
    * Refactor small things

--- packages/core/src/events/get-next-block-range.js
+++ packages/core/src/events/get-next-block-range.js
@@ -4,16 +4,15 @@ const getConfigForEventType = require('./get-config-for-event-type');
 const getLastProcessedBlock = require('./get-last-processed-block');
 
 const getNextBlockRange = async ({
-  currentBlock,
   eventType,
+  maxBlockNumber,
   protocolVersion,
 }) => {
   const config = getConfigForEventType(eventType, protocolVersion);
-  const { maxChunkSize, minConfirmations, startBlock } = config;
+  const { maxChunkSize, startBlock } = config;
   const lastBlock = await getLastProcessedBlock(eventType, protocolVersion);
-  const maxBlock = currentBlock - minConfirmations;
   const fromBlock = lastBlock === null ? startBlock : lastBlock + 1;
-  const toBlock = _.clamp(fromBlock + maxChunkSize, 1, maxBlock);
+  const toBlock = _.clamp(fromBlock + maxChunkSize, 1, maxBlockNumber);
 
   // Notify the consumer that there are no blocks to process
   if (toBlock < fromBlock) {
